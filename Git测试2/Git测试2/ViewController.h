17e4fb3648fa7455211c1651d3cfa3cb2cb336fa
liuwqihao <136990024@qq.com>
2016-01-28 18:14:11 +0800

    第一次

---
+++ Git测试2/Git测试2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git测试2
+//
+//  Created by Xuan on 16/1/28.
+//  Copyright © 2016年 Xuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Git测试2/Git测试2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Git测试2
+//
+//  Created by Xuan on 16/1/28.
+//  Copyright © 2016年 Xuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"11111");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
